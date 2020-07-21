42d3c7cba616bfe38e2c6bb1c55222ced96b7b80
yash-ahir <yashsorathiya25@gmail.com>
2020-07-22 04:23:27 +0530

    Replaced pycrypto with pycryptodome, key and plaintext will be encoded to utf-8 accordingly as to accomodate the change as pycryptodome doesn't accept str objects.

--- src/crypt.py
+++ src/crypt.py
@@ -1,6 +1,8 @@
 from Crypto.Cipher import AES
 from hashlib import pbkdf2_hmac
 from Padding import appendPadding, removePadding
+from os import urandom
+
 
 def _required_padding(a, b = 16, c = 1):
     while a > b:
@@ -13,14 +15,14 @@ def encrypt(plaintext, key, iv):
         plaintext = appendPadding(plaintext, _required_padding(len(plaintext)))
     if len(key) < 32:
         key = appendPadding(key, 32)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    ciphertext = cipher.encrypt(plaintext)
+    cipher = AES.new(key.encode("utf-8"), AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(plaintext.encode("utf-8"))
     return ciphertext
 
 def decrypt(ciphertext, key, iv):
     if len(key) < 32:
         key = appendPadding(key, 32)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+    cipher = AES.new(key.encode("utf-8"), AES.MODE_CBC, iv)
     plaintext = removePadding((cipher.decrypt(ciphertext)).decode(), _required_padding(len(ciphertext)))
     return plaintext
 
